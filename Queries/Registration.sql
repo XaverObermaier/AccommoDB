888bfa2b3621681cfccc19f01ee2fcf604421576
Xaver Obermaier <xaver.obermaier@yahoo.de>
2024-03-30 13:00:35 +0700

    User is now able to create accommodation

--- Queries/Registration.sql
+++ Queries/Registration.sql
@@ -4,8 +4,8 @@ DO $$
 DECLARE
     username_valid BOOLEAN;
     password_valid BOOLEAN;
-    new_username VARCHAR(255) := 'User';  -- Change Username here
-    new_password VARCHAR(255) := 'TestPassword';  -- Change Password here 
+    new_username VARCHAR(255) := 'Xaver';  -- Change Username here
+    new_password VARCHAR(255) := 'Obermaier';  -- Change Password here 
 BEGIN
     -- Check if the username is unique
     username_valid := public.check_unique_username(new_username);
@@ -34,3 +34,5 @@ BEGIN
         END IF;
     END IF;
 END $$;
+
+--
--- Views/login_view.sql
+++ Views/login_view.sql
@@ -1,15 +1,16 @@
 DO $$
 DECLARE
     entered_username VARCHAR(255) := 'User';  -- Test Username here
-    entered_password VARCHAR(255) := 'TestPasword';  -- Test Password here 
+    entered_password VARCHAR(255) := 'TestPassword';  -- Test Password here 
 BEGIN
     EXECUTE '
         CREATE OR REPLACE VIEW public.login_view AS
         SELECT 
             username,
             ''' || entered_password || ''' AS entered_password,
-            public.check_login(''' || entered_username || ''', ''' || entered_password || ''') AS login_success,
-            password_hash AS actual_password
+            password_hash AS actual_password,
+            public.check_login(''' || entered_username || ''', ''' || entered_password || ''') AS login_success
+           
         FROM "public"."user"
     ';
 END $$;
